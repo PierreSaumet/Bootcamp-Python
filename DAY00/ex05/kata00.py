c98499cb361cdb4abf49a5d10a27a4379951385f
Pierre SAUMET <psaumet@e1r2p8.42.fr>
2020-03-09 11:44:36 +0100

    kata00

---
+++ DAY00/ex05/kata00.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python3
+# -*- Coding: UTF-8 -*-
+
+#####################################
+# Programme Python 3 Type           #
+# Autor: Pierre Saumet, Paris, 2020 #
+# Licence: GPL                      #
+#####################################
+
+#####################################
+# Informations:
+"""
+    This is a program created with Python3.
+    It respects the rules of PEP 8.
+    Everythings is in English.
+    Python rocks.
+"""
+import sys
+import string
+
+t = (19, 42, 21)
+
+if __name__ == "__main__":
+    len_t = len(t)
+    string = ""
+    for i in t:
+        string += str(i)
+        string += ", "
+    len_string = len(string)
+    next_string = string[0:len_string-2]
+    print("The {} numbers are: {}".format(len_t, next_string))
