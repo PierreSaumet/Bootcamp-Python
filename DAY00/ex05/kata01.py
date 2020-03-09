a7ba49a9bdc6c3611ad8f82cdf7f18946aac464b
Pierre SAUMET <psaumet@e1r2p8.42.fr>
2020-03-09 11:45:07 +0100

    kata01 and kata02

---
+++ DAY00/ex05/kata01.py
@@ -0,0 +1,29 @@
+#! usr/bin/env python3
+# -*- Coding: UTF-8 -*-
+
+#####################################
+# Programme Python 3 Type           #
+# Autor: Pierre Saumet, Paris, 2020 #
+# Licence: GPL                      #
+#####################################
+
+#####################################
+# Informations:
+"""
+    This is a program created with Python3.
+    It respects the rules of PEP 8.
+    Everythings is in English.
+    Python rocks.
+"""
+import sys
+import string
+
+languages = {
+    'Python': 'Guido van Rossum',
+    'Ruby': 'Yukihiro Matsumoto',
+    'PHP': 'Rasmus Lerdorf',
+    }
+
+if __name__ == "__main__":
+    for key, val in languages.items():
+        print(key, "was created by ", val)
---
+++ DAY00/ex05/kata02.py
@@ -0,0 +1,25 @@
+#! usr/bin/env python3
+# -*- Coding: UTF-8 -*-
+
+#####################################
+# Programme Python 3 Type           #
+# Autor: Pierre Saumet, Paris, 2020 #
+# Licence: GPL                      #
+#####################################
+
+#####################################
+# Informations:
+"""
+    This is a program created with Python3.
+    It respects the rules of PEP 8.
+    Everything is in English.
+    Python rocks.
+"""
+import sys
+import string
+
+
+my_tuple = (3, 30, 2019, 9, 25)
+
+if __name__ == "__main__":
+    print("%.2d/%.2d/%.4d %.2d:%.2d" % my_tuple)
